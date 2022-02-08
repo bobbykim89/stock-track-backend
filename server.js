b07386b8aae30cf4096d8fdaf7e5dc47f6904e0c
Sihun Kim <skim585@asu.edu>
2022-02-08 11:17:59 -0700

    add basic model and dummy schema info, listening to port 4000

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const cors = require('cors')
+const connectDB = require('./database')
+const schema = require('./schema/index')
 
 const app = express()
+
+// Connect to DB
+connectDB()
 
 // Allow cross-origin requests
 app.use(cors())
 
-// Connect to DB
+// Serve app using GraphQL
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }))
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
